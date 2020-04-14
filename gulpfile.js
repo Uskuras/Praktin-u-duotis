77522125d0c80f2b504817314fbf54dae5fd67d9
Uskuras <dariususkuras@gmail.com>
2020-04-14 14:49:36 +0300

    Non-responsive full webpage

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,22 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
-​
+
 gulp.task('serve', gulp.series(function(done) {
-​
+
     browserSync.init({
         server: {
             baseDir: "app"
         }
     });
-​
+
     gulp.watch("app/scss/*.scss", gulp.series('sass'));
     gulp.watch("app/*.html").on('change', browserSync.reload);
-​
+
     done();
 }));
-​
-​
+
+
 gulp.task('sass', gulp.series(function(done) {
 	return gulp.src('app/scss/style.scss')
 		.pipe(sass({
@@ -31,7 +31,7 @@ gulp.task('sass', gulp.series(function(done) {
         .pipe(browserSync.stream());
         done();
 }));
-​
+
 gulp.task('default', gulp.parallel('serve', function () {
     gulp.series('serve')
 }));
